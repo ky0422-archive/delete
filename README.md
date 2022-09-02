2d746d1551344b66c18b692e9fbf84871b7bf01f
tsukiroku <tsukiroku@yahoo.com>
2022-09-02 17:45:24 +0900

    Updates (Refactoring)

--- README.md
+++
@@ -1,1 +0,0 @@
-![image](./img/delete.gif)
--- src/extension.ts
+++ src/extension.ts
@@ -1,21 +1,19 @@
-import * as vscode from 'vscode';
-import * as rimraf from 'rimraf';
+import * as vscode from 'vscode'
+import * as rimraf from 'rimraf'
 
 export const activate = (context: vscode.ExtensionContext) =>
     context.subscriptions.push(
         vscode.commands.registerCommand('delete.delete', () => {
-            if (!vscode.workspace.workspaceFolders)
-                vscode.window.showErrorMessage('No workspace opened.');
+            if (!vscode.workspace.workspaceFolders) vscode.window.showErrorMessage('No workspace opened.')
             else {
                 vscode.workspace.workspaceFolders.map((f) => {
-                    vscode.window.showInformationMessage(`Deleting ${f.name}`);
-                    rimraf(f.uri.fsPath, (e) =>
-                        vscode.window.showErrorMessage(e!.message)
-                    );
-                });
-                vscode.workspace.updateWorkspaceFolders(0, 1);
+                    vscode.window.showInformationMessage(`Deleting ${f.name}`)
+                    rimraf(f.uri.fsPath, (e) => vscode.window.showErrorMessage(e!.message))
+                })
+
+                vscode.workspace.updateWorkspaceFolders(0, 1)
             }
         })
-    );
+    )
 
-export const deactivate = () => {};
+export const deactivate = () => {}
